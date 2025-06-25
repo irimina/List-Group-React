62f18c7c34f30223f8dd5c7de44d23da8c90cd66
Iulian Irimina <iirimina@schools.nyc.gov>
2025-06-25 14:30:54 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import ListGroup from './components/ListGroup';
+
+/*
+//v1
+import Message from './Message';
+function App(){
+  const name= "Julian";
+
+ return (
+  <h1>Hello {name} This is the <Message /> message </h1>
+ );
+
+}
+export default App;
+
+*/
+
+function App(){
+  return <div><ListGroup /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+//function based component approach
+
+//capitalize the first letter of each word
+//this is called PascalCasing
+
+//render an h1 element with the text Hello world
+function Message(){
+    //this is JSX
+    return <span>Hello World</span>;
+
+}
+
+//to use it
+export default Message;
+//next go to the App.tsx component to use this new component 
